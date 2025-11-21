517e057e75737363e3c91e34fd5e2715c4f4d58b
gmand <gmand@DESKTOP-2KO8PEB>
2025-11-21 16:39:09 -0400

    Classe PdvContext

---
+++ PDVLoja/Data/PdvContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; // Add this using directive
+
+using PDVLoja.Models;
+
+namespace PDVLoja.Data
+{
+    public class PdvContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<ItemVenda> ItensVenda { get; set; }
+        public DbSet<UsuarioCaixa> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Use Helpers.ConnectionHelper para connection string
+                optionsBuilder.UseSqlServer(Helpers.ConnectionHelper.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemVenda>()
+                .HasOne(i => i.Venda)
+                .WithMany(v => v.Itens)
+                .HasForeignKey(i => i.IdVenda);
+
+            modelBuilder.Entity<ItemVenda>()
+                .HasOne(i => i.Produto)
+                .WithMany()
+                .HasForeignKey(i => i.IdProduto);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.EstoqueMinimo)
+                .HasDefaultValue(10);
+        }
+    }
+}
