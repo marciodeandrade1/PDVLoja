79fb2c9d22fab796b5f94b90123d2ead3ca330b3
Marcio de Andrade <marcio@requestdev.net>
2025-11-21 20:13:29 -0400

    Primeiros Forms e ajustes no Program.cs

---
+++ PDVLoja/Forms/FrmLogin.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PDVLoja.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDVLoja.Forms
+{
+    public partial class FrmLogin : Form
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly PdvContext _context;
+
+        public FrmLogin(IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+            _serviceProvider = serviceProvider;
+            _context = _serviceProvider.GetService<PdvContext>();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var user = _context.Usuarios.FirstOrDefault(u => u.Nome == txtNome.Text);
+            if (user != null && user.Autenticar(txtSenha.Text))
+            {
+                var frmMenu = _serviceProvider.GetService<FrmMenuPrincipal>();
+                frmMenu.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Credenciais inválidas!");
+            }
+        }
+    }
+}
--- PDVLoja/Program.cs
+++ PDVLoja/Program.cs
@@ -1,3 +1,10 @@
+using PDVLoja.Data;
+using PDVLoja.Forms;
+using PDVLoja.Services;
+using PDVLoja.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 namespace PDVLoja
 {
     internal static class Program
@@ -11,7 +18,24 @@ namespace PDVLoja
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-           // Application.Run(new Form1());
+            // Application.Run(new Form1());
+            // Program.cs (.NET 8 WinForms com Host)
+            // FIX: Remove usage of PDVLoja.CreateBuilder and args, as they do not exist.
+            // Instead, manually configure services as needed.
+            // Connection string centralizada
+            string conn = @"Server=(localdb)\mssqllocaldb;Database=PDV_LojaDB;Trusted_Connection=True;";
+
+            var services = new ServiceCollection();
+            IServiceCollection servicesWithDbContext = services.AddDbContext<PdvContext>(opt => opt.UseSqlServer(conn));
+            services.AddTransient<VendaService>();
+            services.AddTransient<EstoqueService>();
+            services.AddTransient<RelatorioService>();
+            services.AddTransient<PagamentoIntegrator>();
+            services.AddSingleton<DashboardViewModel>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            Application.Run(serviceProvider.GetRequiredService<FrmLogin>());
         }
     }
 }
